198c201bb28f8333114d681a2892884ca8439330
Miko Kiiski <miko.kiiski@reaktor.fi>
2016-08-28 23:40:50 +0300

    Add current time as a sensor

--- src/Sensors.js
+++ src/Sensors.js
@@ -3,18 +3,35 @@ import './Sensors.css';
 
 const h = React.DOM
 
+const pad = str => val => (str + val).slice(-str.length)
+const pad00 = pad('00')
+
 class Sensors extends Component {
   render() {
-    console.log(this.props.sensors)
+
+    var now = new Date()
     return h.ul({ className: 'item-list' },
-      this.props.sensors.map((sensor, index) =>
-        h.li({
+      h.li({
           className: 'sensor-item',
-          key: `sensor-${index}`
+          key: `sensor-time`
         },
-          h.div({ className: 'sensor-item-header' }, sensor.name),
-          h.div({ className: 'sensor-item-content' }, sensor.value)
-        )))
+        h.div({
+            className: 'sensor-item-header',
+            style: {
+              marginTop: 39,
+              top: -14,
+              position: 'relative'
+            }
+          }, (pad00(now.getHours()) + ':' + pad00(now.getMinutes()))
+        )),
+        this.props.sensors.map((sensor, index) =>
+          h.li({
+              className: 'sensor-item',
+              key: `sensor-${index}`
+            },
+            h.div({ className: 'sensor-item-header' }, sensor.name),
+            h.div({ className: 'sensor-item-content' }, sensor.value)
+          )))
   }
 }
 
