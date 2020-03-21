5b2b8d097833888f1b0919c11a55e9df8f7aa187
Miko Kiiski <miko.kiiski@gmail.com>
2020-03-21 20:00:27 +0200

    Remove unused variables

--- src/Sensors.js
+++ src/Sensors.js
@@ -1,13 +1,9 @@
 import React, {Component} from 'react';
 import './Sensors.css';
 
-const pad = str => val => (str + val).slice(-str.length)
-const pad00 = pad('00')
-
 class Sensors extends Component {
   render() {
 
-    var now = new Date()
     return <ul className='item-list'>
       {this.props.sensors.map((sensor, index) =>
         <li className='sensor-item' key={`sensor-${index}`}>
